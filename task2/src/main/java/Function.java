b0c163f3d8c887e3763440c73cc822396f14d689
eilheln <60356138+eilheln@users.noreply.github.com>
2021-10-04 23:59:34 +0300

    Add files via upload

---
+++ task2/src/main/java/Function.java
@@ -0,0 +1,12 @@
+public class Function {
+    public static double calcY(double x){
+        double y = 0;
+        if(x > 3){
+            y = 1/(x*x+1);
+        }else{
+            y = 9;
+        }
+        return y;
+
+    }
+}
